a29bf2318cf073f0e9aae233790ba373d697a46f
Zadie Moon <zadiemoon.println@gmail.com>
2025-10-07 09:42:02 -0700

    update theme

--- theme.js
+++ theme.js
@@ -1,32 +1,3 @@
-// let theme = localStorage.getItem('theme');
-// // let theme = 'dark';
-//
-// window.onload = function() {
-//     if (theme === null) {
-//         theme = 'dark';
-//         localStorage.setItem('theme', theme)
-//     }
-//
-//     document.body.className = `theme-${theme}`;
-//     document.getElementById('switch').checked = (theme === 'light');
-// }
-//
-//
-// console.log(theme);
-//
-// function changeTheme() {
-//     if (theme === 'dark') {
-//         theme = 'light';
-//     } else {
-//         theme = 'dark';
-//     }
-//
-//     document.body.className = `theme-${theme}`;
-//     localStorage.setItem('theme', theme)
-// }
-
-
-
 let theme = localStorage.getItem('theme');
 // let theme = 'dark';
 
